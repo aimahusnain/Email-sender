c5c927783198d0958075afc1c7bd467a63fa0e5e
aimahusnain <aimahusnain@gmail.com>
2024-05-18 21:05:01 +0500

    sdfsdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Email Sender SaaS",
   description: "Email Sender website",
+  themeColor: "#287efd"
 };
 
 export default function RootLayout({
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    {/* <head>
+      <meta></meta>
+    </head> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
